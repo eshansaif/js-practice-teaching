1c72639d8406322d68c270382748efedebda51af
eshansaif <eshansaif1234@gmail.com>
2024-02-11 15:49:43 +0600

    added navbar

---
+++ control-statement/switch.js
@@ -0,0 +1,25 @@
+const letter = prompt(
+  "Please Enter a Letter and Check whether its vowel or consonant"
+);
+
+switch (letter) {
+  case "a":
+    alert("Its a vowel");
+    break;
+  case "e":
+    alert("Its a vowel");
+    break;
+  case "i":
+    alert("Its a vowel");
+    break;
+  case "o":
+    alert("Its a vowel");
+    break;
+  case "u":
+    alert("Its a vowel");
+    break;
+
+  default:
+    alert("Its a consonant");
+    break;
+}
