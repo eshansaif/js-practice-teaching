3fb1c19cac72f6d924067955c2afc6c71ab8e39e
eshansaif <eshansaif1234@gmail.com>
2024-02-13 23:46:58 +0600

    added a validation

--- small-projects/guessing-game/guessing-game.js
+++ small-projects/guessing-game/guessing-game.js
@@ -1,18 +1,26 @@
 document.write(`<h3 style="text-align: center">
 Please Select a Number a between 1 to 5 and see your fate!!
-</h3>;`);
+</h3>`);
 
 let noOfWon = 0;
 let noOfLost = 0;
 for (let i = 1; i <= 5; i++) {
   const userInput = parseInt(prompt("Please Enter a number a between 1-5"));
-  const randomNumber = Math.round(Math.random() * 5);
-  if (userInput === randomNumber) {
-    alert(`Congratulations! You Have Won!!`);
-    noOfWon++;
+
+  if (!userInput) {
+    alert(
+      "You can't keep the prompt empty! To proceed again, please reload the page."
+    );
+    break;
   } else {
-    alert(`Opps! You have lost - The random number was ${randomNumber}!!`);
-    noOfLost++;
+    const randomNumber = Math.round(Math.random() * 5);
+    if (userInput === randomNumber) {
+      alert(`Congratulations! You Have Won!!`);
+      noOfWon++;
+    } else {
+      alert(`Opps! You have lost - The random number was ${randomNumber}!!`);
+      noOfLost++;
+    }
   }
 }
 
